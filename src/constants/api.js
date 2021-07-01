d66e01c1fb2f00ca850a74c76cd209ab228bcfa6
VuongNguyen <a5ntvuong@gmail.com>
2021-07-02 02:00:17 +0700

    add category api for Thong

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,3 +3,4 @@ export const Register_Account_API = "/v1/auth/register";
 export const Login_Account_API = "/v1/auth/login";
 export const Logout_Account_API = "/v1/auth/logout";
 export const Get_Packages_API = "/v1/packages";
+export const Category_API = "/v1/packages?category=";
